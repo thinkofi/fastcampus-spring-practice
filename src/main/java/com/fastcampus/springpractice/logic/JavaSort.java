e445de0d5574554a702317ff255b7286aa9d6c7d
Seongho Ahn <thinkofi@hanmail.net>
2025-12-31 17:09:05 +0900

    Change #1 - package 명 변경 복사를 그대로 했기 때문에 ...

--- src/main/java/com/fastcampus/springpractice/logic/JavaSort.java
+++ src/main/java/com/fastcampus/springpractice/logic/JavaSort.java
@@ -1,9 +1,14 @@
-package com.fastcampus.javaoop.logic;
+package com.fastcampus.springpractice.logic;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+//@Primary
+@Component
 public class JavaSort<T extends Comparable<T>> implements Sort<T> {
     @Override
     public List<T> sort(List<T> list) {
--- src/test/java/com/fastcampus/springpractice/logic/BubbleSortTest.java
+++ src/test/java/com/fastcampus/springpractice/logic/BubbleSortTest.java
@@ -1,9 +1,8 @@
-package com.fastcampus.javaoop.logic;
+package com.fastcampus.springpractice.logic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
